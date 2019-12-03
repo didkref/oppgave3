ffeef3b19fc63011576fc4a97c468d6cb0d5074a
didkref <didrik@krefting.com>
2019-12-03 17:47:06 +0100

    Updates

---
+++ precache-manifest.bf6426343f05c436c8f087627b8254db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868b3b39ce8337cda080188a6c9c3d17",
+    "url": "/oppgave3/index.html"
+  },
+  {
+    "revision": "e6e33b5663a8983f2447",
+    "url": "/oppgave3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7094811ac53354f66753",
+    "url": "/oppgave3/static/js/2.41dab7da.chunk.js"
+  },
+  {
+    "revision": "e6e33b5663a8983f2447",
+    "url": "/oppgave3/static/js/main.9774b5cb.chunk.js"
+  },
+  {
+    "revision": "3e0dc9d1b89a58c9b475",
+    "url": "/oppgave3/static/js/runtime-main.f4448b1d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/oppgave3/static/media/logo.25bf045c.svg"
+  }
+]);
